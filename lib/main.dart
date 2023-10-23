a79492778794e352760f32bd2e123f6b47858503
Harsh <harsh.aggarwal@amadeus.com>
2023-10-23 14:53:03 +0530

    made the favorite page

--- lib/main.dart
+++ lib/main.dart
@@ -51,60 +51,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex=0;
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    
     Widget page;
     switch (selectedIndex) {
       case 0:
         page = const GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = Favourite();
         break;
       default:
         throw UnimplementedError("Invalid index $selectedIndex");
     }
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-            body: Row(
-          children: [
-            // to make sure that notch or something else is not covering the content.NavigationRail
-            SafeArea(
-              child: NavigationRail(
-                  // to hide the label as we do not have enough space.
-                  // extended: false,
-                  extended: constraints.maxWidth >= 600,
-                  destinations: const [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text("Home"),
-                    ),
-                    NavigationRailDestination(
-                        icon: Icon(Icons.favorite),
-                        label: Text(
-                          "favorites",
-                        ))
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) => {
-                        setState(() {
-                          selectedIndex = value;
-                        }),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          body: Row(
+        children: [
+          // to make sure that notch or something else is not covering the content.NavigationRail
+          SafeArea(
+            child: NavigationRail(
+                // to hide the label as we do not have enough space.
+                // extended: false,
+                extended: constraints.maxWidth >= 600,
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text("Home"),
+                  ),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text(
+                        "favorites",
+                      ))
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) => {
+                      setState(() {
+                        selectedIndex = value;
                       }),
+                    }),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primary,
+              child: page,
             ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primary,
-                child: page,
-              ),
-            )
-          ],
-        ));
-      }
+          )
+        ],
+      ));
+    });
+  }
+}
+
+class Favourite extends StatelessWidget {
+  const Favourite({super.key});
+  @override
+  Widget build(BuildContext context) {
+    var appstate = context.watch<MyAppState>();
+
+  if(appstate.favrouiteWords.isEmpty){
+    return const Center(
+      child: Text("No favorites yet 😢"),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have'
+          '${appstate.favrouiteWords.length} favorites:', style: TextStyle(
+            fontSize: 25,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),),
+        ),
+        for( var word in appstate.favrouiteWords)
+          ListTile(
+            leading: Icon(Icons.favorite, color: Colors.pink,),
+            title: Text(word.asPascalCase, style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),),
+          )
+        
+      ],
     );
   }
 }
